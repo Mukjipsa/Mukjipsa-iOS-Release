13eb05f6b7f90abe7c0f25119b1f2d08d87c1520
yungu0010 <dbstj16@sookmyung.ac.kr>
2023-01-28 01:15:49 +0900

    [Add] #3 - UIFont+ 추가

---
+++ Mukjipsa-iOS-Release/Mukjipsa-iOS-Release/Resources/Extension/UIFont+.swift
@@ -0,0 +1,69 @@
+//
+//  UIFont+.swift
+//  Mukjipsa-iOS-Release
+//
+//  Created by 강윤서 on 2023/01/28.
+//
+
+import UIKit
+
+extension UIFont {
+    // Title & Heading
+    class var t1: UIFont {
+        return UIFont(name: "Pretendard-SemiBold", size: 25.0)!
+    }
+    
+    class var h1: UIFont {
+        return UIFont(name: "Pretendard-SemiBold", size: 20.0)!
+    }
+    
+    class var h2: UIFont {
+        return UIFont(name: "Pretendard-SemiBold", size: 18.0)!
+    }
+    
+    class var h3: UIFont {
+        return UIFont(name: "Pretendard-Regular", size: 20.0)!
+    }
+    
+    class var sh1: UIFont {
+        return UIFont(name: "Pretendard-Light", size: 25.0)!
+    }
+    
+    class var sh2: UIFont {
+        return UIFont(name: "Pretendard-Regular", size: 16.0)!
+    }
+    
+    // Body
+    class var bu1: UIFont {
+        return UIFont(name: "Pretendard-SemiBold", size: 16.0)!
+    }
+    
+    class var bu2: UIFont {
+        return UIFont(name: "Pretendard-SemiBold", size: 14.0)!
+    }
+    
+    class var b1: UIFont {
+        return UIFont(name: "Pretendard-SemiBold", size: 13.0)!
+    }
+    
+    class var b2: UIFont {
+        return UIFont(name: "Pretendard-Regular", size: 14.0)!
+    }
+    
+    class var b3: UIFont {
+        return UIFont(name: "Pretendard-Regular", size: 13.0)!
+    }
+    
+    class var b4: UIFont {
+        return UIFont(name: "Pretendard-Light", size: 13.0)!
+    }
+    
+    class var b5: UIFont {
+        return UIFont(name: "Pretendard-Light", size: 10.0)!
+    }
+    
+    // Caption
+    class var caption1: UIFont {
+        return UIFont(name: "Pretendard-SemiBold", size: 10.0)!
+    }
+}
