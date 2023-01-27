69f75507ac2b4b8617ef6e70e5d5bb28aee032ad
yungu0010 <dbstj16@sookmyung.ac.kr>
2023-01-28 00:41:57 +0900

    [Add] #3 - UIColor+ Extension 추가

---
+++ Mukjipsa-iOS-Release/Mukjipsa-iOS-Release/Resources/Extension/UIColor+.swift
@@ -0,0 +1,19 @@
+//
+//  UIColor+.swift
+//  Mukjipsa-iOS-Release
+//
+//  Created by 강윤서 on 2023/01/28.
+//
+
+import UIKit
+
+extension UIColor {
+    static let primary = UIColor(named: "Primary")
+    static let secondary = UIColor(named: "Secondary")
+    static let gray1 = UIColor(named: "Gray1")
+    static let gray2 = UIColor(named: "Gray2")
+    static let gray3 = UIColor(named: "Gray3")
+    static let gray4 = UIColor(named: "Gray4")
+    static let dark = UIColor(named: "Dark")
+    static let white = UIColor(named: "White")
+}
