bbd36e62c048653e6975724c5e2dab74b8dabd54
cchanmi <gkfn980@hs.ac.kr>
2023-01-31 00:33:35 +0900

    [Add] #6 - 로티 파일 상수화

---
+++ Mukjipsa-iOS-Release/Mukjipsa-iOS-Release/Resources/Constant/Lottie.swift
@@ -0,0 +1,14 @@
+//
+//  Lottie.swift
+//  Mukjipsa-iOS-Release
+//
+//  Created by 황찬미 on 2023/01/31.
+//
+
+import Foundation
+
+struct Lottie {
+    static let one = "onboardingStart"
+    static let tow = "onboardingEnd"
+    static let popup = "popup"
+}
