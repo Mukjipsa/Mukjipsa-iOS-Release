a72e2d30e18a4fa2ddd895cec959c6ba713e603c
cchanmi <gkfn980@hs.ac.kr>
2023-01-31 00:30:49 +0900

    [Add] #6 - 이미지 에셋 상수화

--- Mukjipsa-iOS-Release/Mukjipsa-iOS-Release/Resources/Constant/Image.swift
+++ Mukjipsa-iOS-Release/Mukjipsa-iOS-Release/Resources/Constant/Image.swift
@@ -5,4 +5,61 @@
 //  Created by 황찬미 on 2023/01/31.
 //
 
-import Foundation
+import UIKit
+
+struct Image {
+    
+    // MARK: - Tabbar
+    
+    static let tabbarBookmark = UIImage(named: "tabbarBookmark")
+    static let tabbarBookmarkSelected = UIImage(named: "tabbarBookmarkSelected")
+    static let tabbarRecipe = UIImage(named: "tabbarRecipe")
+    static let tabbarRecipeSelected = UIImage(named: "tabbarRecipeSelected")
+    static let tabbarIngredient = UIImage(named: "tabbarIngredient")
+    static let tabbarIngredientSelected = UIImage(named: "tabbarIngredientSelected")
+    
+    // MARK: - logo
+    
+    static let logo = UIImage(named: "logo")
+    static let logoLogin = UIImage(named: "logoLogin")
+    static let logoSplash = UIImage(named: "logoSplash")
+    
+    // MARK: - Img
+    
+    static let imgEmptyBookmark = UIImage(named: "imgEmptyBookmark")
+    static let imgEmptySearch = UIImage(named: "imgEmptySearch")
+    static let imgEmptyRecipe = UIImage(named: "imgEmptyRecipe")
+    
+    // MARK: - Btn
+    
+    static let btnPossible = UIImage(named: "btnPossible")
+    static let btnPossibleSelected = UIImage(named: "btnPossibleSelected")
+    static let btnBookmark = UIImage(named: "btnBookmark")
+    static let btnBookmarkSelected = UIImage(named: "btnBookmarkSelected")
+    
+    // MARK: - ProgressBar
+    
+    static let progressBarImageOne = UIImage(named: "progressBarOne")
+    static let progressBarImageTwo = UIImage(named: "progressBarTwo")
+    static let progressBarImageThree = UIImage(named: "progressBarThree")
+    static let progressBarImageFour = UIImage(named: "progressBarFour")
+    
+    // MARK: - Ingredient Icn
+    
+    static let icnMeat = UIImage(named: "icnMeat")
+    static let icnFish = UIImage(named: "icnFish")
+    static let icnVege = UIImage(named: "icnVege")
+    static let icnGrain = UIImage(named: "icnVege")
+    static let icnSeasoning = UIImage(named: "icnSeasoning")
+    static let icnCheeze = UIImage(named: "icnCheeze")
+    static let icnBread = UIImage(named: "icnBread")
+    static let icnEtc = UIImage(named: "icnEtc")
+    
+    // MARK: - etc Icn
+    
+    static let icnSearch = UIImage(named: "icnSearch")
+    static let icnDelete = UIImage(named: "icnDelete")
+    static let icnBack = UIImage(named: "icnBack")
+    static let icnCheck = UIImage(named: "icnCheck")
+    
+}
