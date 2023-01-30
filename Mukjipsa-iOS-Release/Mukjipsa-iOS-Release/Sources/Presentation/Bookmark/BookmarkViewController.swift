45d733ed0e80dc749a7d4ae19ad28edf8aa9198a
cchanmi <gkfn980@hs.ac.kr>
2023-01-31 00:41:13 +0900

    [Feat] #6 - 탭바 세팅

---
+++ Mukjipsa-iOS-Release/Mukjipsa-iOS-Release/Sources/Presentation/Bookmark/BookmarkViewController.swift
@@ -0,0 +1,15 @@
+//
+//  BookmarkViewController.swift
+//  Mukjipsa-iOS-Release
+//
+//  Created by 황찬미 on 2023/01/31.
+//
+
+import UIKit
+
+final class BookmarkViewController: ViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ Mukjipsa-iOS-Release/Mukjipsa-iOS-Release/Sources/Presentation/MyRecipe/MyRecipeViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MyRecipeViewController.swift
+//  Mukjipsa-iOS-Release
+//
+//  Created by 황찬미 on 2023/01/31.
+//
+
+import UIKit
+
+final class MyRecipeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
