4023dd3e182f94dce56eb9610daaf3992581c16b
yungu0010 <dbstj16@sookmyung.ac.kr>
2023-03-06 00:30:32 +0900

    [Fix] #14 - rootViewController 스플래쉬 뷰로 변경

--- Mukjipsa-iOS-Release/Mukjipsa-iOS-Release/Resources/Support/SceneDelegate.swift
+++ Mukjipsa-iOS-Release/Mukjipsa-iOS-Release/Resources/Support/SceneDelegate.swift
@@ -13,9 +13,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let scene = (scene as? UIWindowScene) else { return }
-        self.window = UIWindow(windowScene: scene)
-        self.window?.rootViewController = TabBarController()
-        self.window?.makeKeyAndVisible()
+        window = UIWindow(windowScene: scene)
+        window?.rootViewController = SplashViewController()
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) { }
